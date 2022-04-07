7fb69d142d5e9124bb3e0f02a542d5fcb2087900
richardvlucena <richard.v.lucena@protonmail.com>
2022-04-07 13:08:01 -0300

    frozen quiz

---
+++ python/frozen_Quiz.py
@@ -0,0 +1,205 @@
+print('''
+            OLÁ, BEM VINDO AO ❄ FROZEN QUIZ ❄️!  O QUIZ SOBRE A FRANQUIA DE FILMES/CURTAS FROZEN, DA DISNEY.
+      ''')
+
+print(''' 
+                                          VAMOS EMBARCAR EM UMA AVENTURA CONGELANTE 🥶
+     ''')
+pontos = 0
+
+print("PERGUNTA N° 1)  Qual era o nome da melhor amiga de infancia da Anna ?")
+print("(a) Serisa")
+print("(b) Corine")
+print("(c) Elsa")
+print("(d) Merlia")
+
+resposta = input("DIGITE AQUI :  ")
+
+if resposta =="C" or resposta == "c":
+    print("ACERTOU")
+    pontos += 1
+    print("sua pontuação atual é de : {}" .format(pontos))
+else:
+    print("ERROU")
+
+print('''
+❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄
+    ''')
+
+print("PERGUNTA N° 2)  qual é o nome completo da Elsa ?")
+print("(a) Elsa de Arendelle ")
+print("(b) Elsa das Ilhas do Sul ")
+print("(c) Elsa de Andalasia")
+print("(d) Elsa de Marmoreal")
+
+resposta = input("DIGITE AQUI :  ")
+
+if resposta =="a" or resposta == "A":
+    print("ACERTOU")
+    pontos += 1
+else:
+    print("ERROU")
+
+print('''
+❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄
+    ''')
+print("PERGUNTA N° 3)  Anna e Elsa são : ")
+print("(a) Primas ")
+print("(b) Colegas ")
+print("(c) Inimigas")
+print("(d) Irmãs")
+
+resposta = input("DIGITE AQUI :  ")
+
+if resposta == "d" or resposta == "D":
+    print("ACERTOU")
+    pontos += 1
+else:
+    print("ERROU")
+
+print('''
+❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄
+    ''')
+print("PERGUNTA N° 4)  Arendelle está localizado em qual país ?")
+print("(a) Canadá")
+print("(b) Noruega ")
+print("(c) Suiça")
+print("(d) Russia")
+
+resposta = input("DIGITE AQUI :  ")
+
+if resposta == "b" or resposta == "B":
+    print("ACERTOU")
+    pontos += 1
+else:
+    print("ERROU")
+
+print('''
+❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄
+    ''')
+print("PERGUNTA N° 5)  Quem estava fazendo aniversário no curta Frozen Febre Congelante ?")
+print("(a) Iduna")
+print("(b) Olaf ")
+print("(c) Anna")
+print("(d) Elsa")
+
+resposta = input("DIGITE AQUI :  ")
+
+if resposta == "c" or resposta == "C":
+    print("ACERTOU")
+    pontos += 1
+else:
+    print("ERROU")
+
+print('''
+❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄
+    ''')
+print("PERGUNTA N° 6)  Qual era a Tradição de Natal de Anna e Elsa ?")
+print("(a) Tocar o sino")
+print("(b) Jantar em Família ")
+print("(c) Olaf")
+print("(d) Sven")
+
+resposta = input("DIGITE AQUI :  ")
+
+if resposta == "c" or resposta == "C":
+    print("ACERTOU")
+    pontos += 1
+else:
+    print("ERROU")
+
+print('''
+❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄
+    ''')
+print('PERGUNTA N° 7)   A Franquia Frozen é composta por quantos ''Filmes''? (obs:  Filmes inclui :  Filmes,Curtas e Séries) ')
+print("(a) 9")
+print("(b) 4 ")
+print("(c) 10")
+print("(d) 7")
+
+resposta = input("DIGITE AQUI :  ")
+
+if resposta == "a" or resposta == "A":
+    print("ACERTOU")
+    pontos += 1
+else:
+    print("ERROU")
+
+print('''
+❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄
+    ''')
+print('PERGUNTA N° 8)   Bruni é o espirito de qual elemento ?')
+print("(a) Água")
+print("(b) Ar ")
+print("(c) Fogo")
+print("(d) Terra")
+
+resposta = input("DIGITE AQUI :  ")
+
+if resposta == "c" or resposta == "C":
+    print("ACERTOU")
+    pontos += 1
+else:
+    print("ERROU")
+
+print('''
+❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄
+    ''')
+
+print('PERGUNTA N° 9)   Qual a estação do ano favorita de Olaf ?')
+print("(a) Inverno")
+print("(b) Verão ")
+print("(c) Outono")
+print("(d) Primavera")
+
+resposta = input("DIGITE AQUI :  ")
+
+if resposta == "b" or resposta == "B":
+    print("ACERTOU")
+    pontos += 1
+else:
+    print("ERROU")
+
+print('''
+❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄
+    ''')
+print('PERGUNTA N° 10 (ULTIMA)  Quem é a Atual Rainha de Arendelle ?')
+print("(a) Elsa")
+print("(b) Anna ")
+print("(c) Iduna")
+print("(d) Atina")
+
+resposta = input("DIGITE AQUI :  ")
+
+if resposta == "b" or resposta == "B":
+    print("ACERTOU")
+    pontos += 1
+else:
+    print("ERROU")
+
+print('''
+                                                OBRIGADO POR JOGAR O ❄ FROZEN QUIZ ❄️! 
+      ''')
+
+pontost = pontos
+
+if pontost >= 9: # 9 e 10
+    print("Total de pontos : {}".format(pontost))
+    print("VOCE EH UM EXPERT NO ASSUNTO")
+
+elif pontost >6 and pontos <9: # 7 e 8
+    print("Total de pontos : {}".format(pontost))
+    print("VOCE TEM UM BOM CONHECIMENTO DO ASSUNTO")
+
+elif pontost >4 and pontos <7: # 5 e 6
+    print("Total de pontos : {}".format(pontost))
+    print("VOCE TEM UM CERTO CONHECIMENTO DO ASSUNTO")
+
+elif pontost >2 and pontos <5: # 3 e 4
+    print("Total de pontos : {}".format(pontost))
+    print("SEU CONHECIMENTO EH BASICO")
+
+else:
+    print("Total de pontos : {}".format(pontost))
+    print("VOCE EH UM NOOB NESTE ASSUNTO")
+
